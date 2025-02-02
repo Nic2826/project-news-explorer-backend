37e618b60407c2863e3c277696dc86ddbf231e97
Nic2826 <nicolalgarin2605@gmail.com>
2025-02-02 16:19:27 -0500

    finished

--- routes/article.js
+++ routes/article.js
@@ -1,7 +1,6 @@
 // routes/articles.js
 const router = require('express').Router();
 const { getArticles, createArticle, deleteArticle } = require('../controllers/articles');
-const { auth } = require('../middleware/auth');
 
 router.get('/articles', auth, getArticles);
 router.post('/articles', auth, createArticle);
